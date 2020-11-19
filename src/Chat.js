1b05dc67888b62b4c8f93673107ae4645b61260a
Archit1607 <Archit778@gmail.com>
2020-11-19 20:02:56 +0530

    basic functionality over

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Chat.css';
 import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -8,31 +8,42 @@ import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 import { useParams } from 'react-router-dom';
 import db from './Firebase';
+import { userContext } from './UserContext';
+import firebase from "firebase";
 
 function Chat() {
 
-    const {roomId} = useParams();
+    const currentUser = useContext(userContext);
+    const { roomId } = useParams();
     const [roomName, setRoomName] = useState("")
     const [seed, setSeed] = useState('');
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, [roomId])
 
     useEffect(() => {
-        if(roomId){
-            db.collection("rooms").doc(roomId).onSnapshot(snapshot =>(
+        if (roomId) {
+            db.collection("rooms").doc(roomId).onSnapshot((snapshot) => (
                 setRoomName(snapshot.data().name
-                    )
-            ))
+                )
+            ));
+
+            db.collection("rooms").doc(roomId).collection("messages").orderBy('timestamp', 'asc').onSnapshot(snapshot => (setMessages(snapshot.docs.map(doc => doc.data()))))
         }
     }, [roomId])
 
-const sendMessage = (e) =>{
-e.preventDefault();
-console.log("you typed", input);
-setInput('');
+    const sendMessage = (e) => {
+        e.preventDefault();
+        db.collection("rooms").doc(roomId).collection("messages").add({
+            name:currentUser.displayName,
+            message:input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            
+        }) 
+        setInput('');
     }
 
 
@@ -43,7 +54,12 @@ setInput('');
 
                 <div className="chat__headerInfo">
                     <h3>{roomName}</h3>
-                    <p>Last Seen...</p>
+                    <p>Last Seen
+                        {" "}
+                        {
+                            new Date(messages[messages.length -1]?.timestamp?.toDate()).toUTCString()
+                        } 
+                    </p>
                 </div>
                 <div className="chat__headerRight">
                     <IconButton>
@@ -59,20 +75,24 @@ setInput('');
             </div>
 
             <div className="chat__body">
-                <p className={`chat__message ${true &&"chat__reciever"}`}><span className="chat__name">Sunny</span>Hey Guys
-                <span className="chat__timestamp">3:552pm</span>
-                </p>
+                {messages.map((message) => (
+                    <p className={`chat__message ${
+                        message.name === currentUser.displayName && "chat__reciever"}`}><span className="chat__name">{message.name}</span>{message.message}
+                        <span className="chat__timestamp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
+                    </p>
+                ))}
+
 
             </div>
 
             <div className="chat__footer">
-                    <InsertEmoticonIcon />
-                    <form>
-                        <input  type ="text" placeholder =" type a message" value = {input} onChange = {(e) => setInput(e.target.value)}
-                        />
-                        <button type ="submit" onClick ={sendMessage}>Send a message</button>
-                    </form>
-                    <MicIcon />
+                <InsertEmoticonIcon />
+                <form>
+                    <input type="text" placeholder=" type a message" value={input} onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button type="submit" onClick={sendMessage}>Send a message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )
