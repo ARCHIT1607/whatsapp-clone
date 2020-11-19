255b192ff26b24b5b38ae0d341373bd5925ed93b
Archit1607 <Archit778@gmail.com>
2020-11-19 16:56:26 +0530

    commit for creating all components and sidebar

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+  const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyC_C5wkDOKqEneDKBDDYTIv6Vtlc36JUDw",
+  authDomain: "whatsapp-clone-18475.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-18475.firebaseio.com",
+  projectId: "whatsapp-clone-18475",
+  storageBucket: "whatsapp-clone-18475.appspot.com",
+  messagingSenderId: "971628563389",
+  appId: "1:971628563389:web:c224ee6ee35862ad00a0d4"
+  });
+
+  const db =  firebaseApp.firestore();
+  export  {firebaseApp, db};
---
+++ src/SidebarChat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SidebarChat.css'
+
+
+function SidebarChat() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SidebarChat
